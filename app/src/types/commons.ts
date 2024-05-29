e8dff3c867b7fe00fa246f83ffd2dd231616cfd7
axolotle <nchesnais@mailoo.org>
2024-05-30 00:42:53 +0200

    refactor: quickly turn components to script setup + ts

---
+++ app/src/types/commons.ts
@@ -0,0 +1,22 @@
+export type Obj<T = any> = Record<string, T>
+
+// Vue
+
+export type VueClass =
+  | string
+  | Record<string, boolean>
+  | (string | Record<string, boolean>)[]
+
+// BVN (not exported types for now)
+
+// eslint-disable-next-line prettier/prettier
+type ColsNumbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12'
+export type Cols = Partial<Record<Breakpoint, boolean | ColsNumbers | 'auto'>>
+
+// CUSTOM
+
+export type CustomRoute = {
+  to: RouteLocationRaw
+  text: string
+  icon?: string
+}

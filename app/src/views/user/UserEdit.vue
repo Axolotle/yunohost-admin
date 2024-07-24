b259f59ccbe31c2ff3f1012b8b2622ec7c77d1b3
axolotle <nchesnais@mailoo.org>
2024-07-24 16:40:41 +0200

    ts: type core data formating functions

--- app/src/views/user/UserEdit.vue
+++ app/src/views/user/UserEdit.vue
@@ -19,7 +19,7 @@ import {
   sameAs,
 } from '@/helpers/validators'
 import {
-  adressToFormValue,
+  formatAdress,
   formatFormData,
   sizeToM,
 } from '@/helpers/yunohostArguments'
@@ -157,10 +157,10 @@ const serverError = ref('')
 
 function onQueriesResponse(user_: any) {
   form.value.fullname = user_.fullname
-  form.value.mail = adressToFormValue(user_.mail)
+  form.value.mail = formatAdress(user_.mail)
   if (user_['mail-aliases']) {
     form.value.mail_aliases = user_['mail-aliases'].map((mail) =>
-      adressToFormValue(mail),
+      formatAdress(mail),
     )
   }
   if (user_['mail-forward']) {
